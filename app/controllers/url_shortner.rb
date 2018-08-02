f2f3e3308d0f4c5ee52a307c63b2dd9f17a4d8e4
Nidhin D <dnidhin5@gmail.com>
2018-08-02 10:24:40 +0530

    Form added

--- app/controllers/url_shortner.rb
+++ app/controllers/url_shortner.rb
@@ -2,7 +2,16 @@ require './app/models/url'
 
 class UrlShortner < Sinatra::Base
     get '/' do
-        @urls = Url.all
         erb :index
     end
+
+    get '/:short_code' do
+        #Redirect to original url
+    end
+
+    post '/' do
+        #Save Data to Url
+        #Call Serivice for shortening
+    end
+
 end
